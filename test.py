3075aced52c3702a8cd6abc336e6083ad4e46df3
Ahmedsabry11 <ahmedsabry232345@gmail.com>
2023-11-03 21:08:01 +0200

    add csv file

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 # test file 
+import numpy as np
+import csv
+import pandas as pd
 
 def main():
     print("Hello World!")
 
 if __name__ == "__main__":
     main()
+    # save array of vector to csv file
