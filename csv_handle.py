9521edeee764dc6aa2c3fe2a3dc25348009a49de
Ahmedsabry11 <ahmedsabry232345@gmail.com>
2023-11-03 21:52:27 +0200

    add: creating csv file

--- csv_handle.py
+++ csv_handle.py
@@ -1,21 +1,24 @@
 # Calculate the byte offset of a specific row in a CSV file with fixed-size rows
 def calculate_byte_offset(line_number, row_size):
     # Calculate the byte offset based on line number and row size
-    byte_offset = (line_number - 1) * row_size
-    return byte_offset
+    return (line_number - 1) * row_size
+
 
 # Assuming you know the line number and the size of each row in bytes
-line_number = 5  # Replace this with the desired line number
-row_size = 100  # Replace this with the size of each row in bytes
+line_number = 10  # Replace this with the desired line number
+row_size = 80*8  # Replace this with the size of each row in bytes
 
 # Calculate the byte offset of the specific row
 byte_offset = calculate_byte_offset(line_number, row_size)
 
 # Open the CSV file and move to the specific byte offset
-with open('large_csv_file.csv', 'r', encoding='utf-8') as csv_file:
+with open('test.csv', 'r', encoding='utf-8') as csv_file:
     # Move to the calculated byte offset in the file
     csv_file.seek(byte_offset)
     # Read the row at the specified byte offset
     specific_row = csv_file.readline().strip()
+    # read from the byte offset 5 more rows
+    for i in range(5):
+        specific_row += csv_file.readline().strip()
 
 print(specific_row)
--- test.py
+++ test.py
@@ -3,9 +3,40 @@ import numpy as np
 import csv
 import pandas as pd
 
-def main():
-    print("Hello World!")
+# test fixed row size in csv
+def insert():
+    m = 10
+    # create numpy array with 1000 rows and 70 columns with type float32
+    embeds = np.random.random((m, 70)).astype(np.float32)
+    # add id column to the numpy array starting from 1
+    ids = np.arange(1, m+1).reshape(-1, 1)
+    # concatenate the id column to the numpy array
+    embeds = np.concatenate((ids, embeds), axis=1).astype(np.float32)
+    # open csv file and write the numpy array to it as numbers after last row without empty line
+    with open("test.csv", "a+") as fout:
+        np.savetxt(fout, embeds, delimiter=",", fmt="%f")
+def load():
+    # load csv file as numpy array
+    embeds = np.loadtxt("test.csv", delimiter=",")
+    # get the id column
+    ids = embeds[:, 0]
+    # get the embed columns
+    embeds = embeds[:, 1:]
+    # print the numpy array
+    print("embeddings",embeds)
+    print("ids",ids)
+    print("shape of embeddings",embeds.shape)
+    print("shape of ids",ids.shape)
+    print("type of embeddings",embeds.dtype)
+    print("type of ids",ids.dtype)
+    
 
+
+
+def main():
+    # print("Hello World!")
+    insert()
+    load()
 if __name__ == "__main__":
     main()
-    # save array of vector to csv file
+    
