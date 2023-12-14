f4fba487f2f09536e2c965903d9a59b0ed68d34b
Ahmedsabry11 <ahmedsabry232345@gmail.com>
2023-12-14 15:09:28 +0200

    add load pq file

--- PQ.py
+++ PQ.py
@@ -388,22 +388,25 @@ class CustomIndexPQ:
         # return np.array(distances_all[:k,0]).astype(np.int32)
 
     def load_file(self,file):
-        loaded_data = []
-        # Loop through the file until the end
-        while True:
-            # Read an integer (4 bytes)
-            packed_integer = file.read(4)
-            if not packed_integer:
-                # Break the loop if there is no more data to read
-                break
-            integer_value = struct.unpack('i', packed_integer)[0]
+        # loaded_data = []
+        # # Loop through the file until the end
+        # while True:
+        #     # Read an integer (4 bytes)
+        #     packed_integer = file.read(4)
+        #     if not packed_integer:
+        #         # Break the loop if there is no more data to read
+        #         break
+        #     integer_value = struct.unpack('i', packed_integer)[0]
 
-            # Read a list of floats (8 bytes each)
-            packed_floats = file.read(4 *self.m)  # Assuming a list of 3 floats per row
-            float_values = [struct.unpack('i', packed_floats[i:i+4])[0] for i in range(0, len(packed_floats), 4)]
-            float_values.insert(0,integer_value)
-            # Append the result as a tuple (integer, list of floats)
-            loaded_data.append(float_values)
+        #     # Read a list of floats (8 bytes each)
+        #     packed_floats = file.read(4 *self.m)  # Assuming a list of 3 floats per row
+        #     float_values = [struct.unpack('i', packed_floats[i:i+4])[0] for i in range(0, len(packed_floats), 4)]
+        #     float_values.insert(0,integer_value)
+        #     # Append the result as a tuple (integer, list of floats)
+        #     loaded_data.append(float_values)
+        
+        loaded_data = np.fromfile(file, dtype=np.int32)
+        loaded_data = loaded_data.reshape((-1, 1+self.m))
         return np.array(loaded_data)
     
     def search_using_IVF(self, Xq: np.ndarray,centriods:np.ndarray,k: int,refine:bool = True) -> tuple[np.ndarray, np.ndarray]:
